1ee33e1a3226f080bc0c962b4b2d3e25858326bf
Spencer Hewitt <spencerhewitt@dhcp-38-56.mil.secure.int.wireless.unca.edu>
2019-04-29 02:19:30 -0400

    started some documentation for final project

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 /* script.js */
+
+
 var i = 0;
 var txt = 'Cybersecurity Enthusiast & Web Developer';
-var speed = 70;
+var speed = 70; /* sets speed of typing effect */
 
 function typewriter_effect() {
     
+    /* loop that prints out the txt var letter by letter */
     if (i < txt.length) {
         document.getElementById('typewriter').innerHTML += txt.charAt(i);
         i++;
@@ -13,3 +16,6 @@ function typewriter_effect() {
 
 
 }
+
+
+
