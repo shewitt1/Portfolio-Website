2a49e305e96ceb7d22eb43e6ca529aabe39b88c3
Spencer Hewitt <shewitt@unca.edu>
2019-04-30 01:01:45 -0400

    formatted code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 /* script.js */
 
-
 var i = 0;
-var txt = 'Cybersecurity Enthusiast & Web Developer';
+var txt = "Cybersecurity Enthusiast & Web Developer";
 var speed = 70; /* sets speed of typing effect */
 
-
 function typewriter_effect() {
-    
-    /* loop that prints out the txt var letter by letter */
-    if (i < txt.length) {
-        document.getElementById('typewriter').innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typewriter_effect, speed);
-    }
-
-
+  /* loop that prints out the txt var letter by letter */
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typewriter_effect, speed);
+  }
 }
-
-
-
-
