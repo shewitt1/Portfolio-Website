0b923fc593744caac15e861293ce8b1a7f52176f
Spencer Hewitt <spencerhewitt@peenpirate.attlocal.net>
2019-05-01 04:27:51 -0400

    added unfinished mode option for day/nightmode

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ var i = 0;
 var txt = "Cybersecurity Enthusiast & Web Developer";
 var speed = 70; /* sets speed of typing effect */
 
+var Date = new Date();
+var hour = Date.getHours()
+
 function typewriter_effect() {
   /* loop that prints out the txt var letter by letter */
   if (i < txt.length) {
@@ -11,4 +14,16 @@ function typewriter_effect() {
     i++;
     setTimeout(typewriter_effect, speed);
   }
+}
+
+function daymode() {
+    document.getElementById('sections').style.backgroundColor == "blue";
+}
+
+function nightmode() {
+
+}
+
+function checktime() {
+
 }
